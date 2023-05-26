ee90e95e54e55c43ff861e24ac588af1a977af6a
LilNeozZ <77625632+LilNeozZ@users.noreply.github.com>
2023-05-26 14:55:57 +0200

    test

---
+++ Book.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public class Book {
+    private String title;
+    private String author;
+    private String publicationDate;
+
+    public Book(String title, String author, String publicationDate) {
+        this.title = title;
+        this.author = author;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+}
