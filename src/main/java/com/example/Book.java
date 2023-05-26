e5ec548b9366b0bd85bdb73041ee33ee802c8ad9
LilNeozZ <77625632+LilNeozZ@users.noreply.github.com>
2023-05-26 15:45:04 +0200

    test

--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -15,23 +15,12 @@ public class Book {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getAuthor() {
         return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
     }
 
     public String getPublicationDate() {
         return publicationDate;
     }
 
-    public void setPublicationDate(String publicationDate) {
-        this.publicationDate = publicationDate;
-    }
 }
--- src/main/java/com/example/BookTest.java
+++ src/main/java/com/example/BookTest.java
@@ -1,14 +1,10 @@
 package com.example;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.util.stream.Stream;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
 public class BookTest {
     private static Stream<Book> bookProvider() {
         return Stream.of(
@@ -17,25 +13,19 @@ public class BookTest {
                 new Book("Title 3", "Author 3", "2023-03-03")
         );
     }
-
     @ParameterizedTest
     @MethodSource("bookProvider")
     void getTitle(Book book) {
         assertThat(book.getTitle(), is(notNullValue()));
     }
-
     @ParameterizedTest
     @MethodSource("bookProvider")
     void getAuthor(Book book) {
         assertThat(book.getAuthor(), is(notNullValue()));
     }
-
     @ParameterizedTest
     @MethodSource("bookProvider")
     void getPublicationDate(Book book) {
         assertThat(book.getPublicationDate(), is(notNullValue()));
     }
-
-    // Ajoutez d'autres méthodes de test si nécessaire
 }
-
